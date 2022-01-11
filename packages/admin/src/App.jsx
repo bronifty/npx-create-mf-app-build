08b1c64aa499fd19338a7301f64071977ccc63e2
bronifty <bronifty@gmail.com>
2022-01-11 13:14:31 -0500

    .

--- packages/admin/src/App.jsx
+++ packages/admin/src/App.jsx
@@ -1,14 +1,27 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import "./index.scss";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useParams,
+} from 'react-router-dom';
+import { useQuery } from 'react-query';
 
 const App = () => (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <div>Name: admin</div>
-    <div>Framework: react</div>
-    <div>Language: JavaScript</div>
-    <div>CSS: Tailwind</div>
-  </div>
+  <Router>
+    <div className='max-w-7xl mx-auto'>
+      <header className='bg-blue-700 text-white w-full font-bold text-3xl'>
+        <h1 className='p-5 text-center'>CMS Editor</h1>
+      </header>
+      <div className='mt-10 text-3xl'>
+        <Routes>
+          <Route path='/:page' element={<div>About</div>} />
+          <Route path='/' element={<div>Home</div>} />
+        </Routes>
+      </div>
+    </div>
+  </Router>
 );
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById('app'));
--- packages/admin/webpack.config.js
+++ packages/admin/webpack.config.js
@@ -16,12 +16,35 @@ module.exports = {
   },
 
   devServer: {
-    before: function (app) {
+    onAfterSetupMiddleware: function (devServer) {
+      const { app } = devServer;
       const bodyParser = require('body-parser');
       app.use(bodyParser.json());
       app.use(require('cors')());
+      app.post('/api/:page', function (req, res) {
+        const page = req.params.page;
+        db.find({ _id: page }, (err, docs) => {
+          if (!err && docs.length > 0) {
+            db.update({ _id: page }, { $set: req.body }, {}, () => {
+              res.json({ record: 'updated' });
+            });
+          } else {
+            db.insert({ _id: page, ...req.body }, () => {
+              res.json({ record: 'inserted' });
+            });
+          }
+        });
+      });
+
       app.get('/api/:page', function (req, res) {
         const page = req.params.page;
+        db.find({ _id: page }, (err, docs) => {
+          if (!err) {
+            res.json(docs[0] || {});
+          } else {
+            res.json({ error: `error getting ${page}` });
+          }
+        });
       });
     },
     port: 8081,
